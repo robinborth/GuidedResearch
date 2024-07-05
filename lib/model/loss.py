fix: gradients and JTF is now the same

lib/model/loss.py
@@ -102,7 +102,7 @@ class BaseLoss(nn.Module):
             p_names=self.p_names,
         )
 
-    def _loss_step(
+    def _residual_step(
         self,
         model: FLAME,
         batch: dict,
@@ -115,14 +115,20 @@ class BaseLoss(nn.Module):
         return loss.reshape(-1)  # (C,)
 
     def loss_step(self):
-        loss = self._loss_step(
+        residuals = self._residual_step(
             model=self.model,
             batch=self.batch,
             correspondences=self.correspondences,
             params=self.params,
             p_names=self.p_names,
         )
-        return dict(loss=loss)
+
+        # for regularization we don't have a residual and just return the empty tensor
+        if not residuals.numel():
+            return {"loss": residuals, self.name: residuals}
+
+        loss = residuals**2
+        return {"loss": loss, self.name: loss}
 
     def _jacobian_step(
         self,
@@ -133,7 +139,7 @@ class BaseLoss(nn.Module):
         p_names: list[str],
     ):
         def closure(*args):
-            return self._loss_step(model, batch, correspondences, args, p_names)
+            return self._residual_step(model, batch, correspondences, args, p_names)
 
         jacobians = torch.autograd.functional.jacobian(
             func=closure,
@@ -143,7 +149,7 @@ class BaseLoss(nn.Module):
             vectorize=True,
         )
         J = torch.cat([j.flatten(-2) for j in jacobians], dim=-1)  # (M, N)
-        F = self._loss_step(model, batch, correspondences, params, p_names)
+        F = self._residual_step(model, batch, correspondences, params, p_names)
         return J, F
 
     def jacobian_step(self):
@@ -160,7 +166,7 @@ class BaseLoss(nn.Module):
     ####################################################################################
 
     def loss_closure(self):
-        return lambda: self.loss_step()["loss"].mean()
+        return lambda: self.loss_step()["loss"].sum()  # sum of the squared residuals
 
     def jacobian_closure(self):
         return lambda: self.jacobian_step()
@@ -215,29 +221,35 @@ class ChainedLoss(BaseLoss):
 # Dense Loss Terms
 ####################################################################################
 class Point2PlaneLoss(BaseLoss):
+    name: str = "point2plane"
+
     def forward(self, out: dict[str, torch.Tensor]):
         point = out["point"]
         point_gt = out["point_gt"]
         normal = out["normal"]
         plane_dist = ((point - point_gt) * normal).sum(-1)
-        return plane_dist**2
+        return plane_dist
 
 
 class Point2PointLoss(BaseLoss):
+    name: str = "point2point"
+
     def forward(self, out: dict[str, torch.Tensor]):
         point = out["point"]
         point_gt = out["point_gt"]
-        return (point - point_gt) ** 2
+        return point - point_gt
 
 
 class SymmetricICPLoss(BaseLoss):
+    name: str = "symmetricICP"
+
     def forward(self, out: dict[str, torch.Tensor]):
         point = out["point"]
         point_gt = out["point_gt"]
         normal = out["normal"]
         normal_gt = out["normal_gt"]
         plane_dist = ((point - point_gt) * (normal + normal_gt)).sum(-1)
-        return plane_dist**2
+        return plane_dist
 
 
 ####################################################################################
@@ -246,6 +258,8 @@ class SymmetricICPLoss(BaseLoss):
 
 
 class LatentRegularizationLoss(BaseLoss):
+    name: str = "regularization"
+
     def forward(self, out: dict[str, torch.Tensor]):
         device = out["point"].device
         shape_params = out["shape_params"]
@@ -261,7 +275,7 @@ class LatentRegularizationLoss(BaseLoss):
             return torch.tensor([], device=device)
 
         latents = torch.cat(params)
-        return latents**2
+        return latents
 
 
 ####################################################################################
@@ -270,14 +284,18 @@ class LatentRegularizationLoss(BaseLoss):
 
 
 class Landmark2DLoss(BaseLoss):
+    name: str = "landmark2d"
+
     def forward(self, out: dict[str, torch.Tensor]):
         lm_2d_ndc = out["lm_2d_ndc"]
         lm_2d_ndc_gt = out["lm_2d_ndc_gt"]
-        return (lm_2d_ndc - lm_2d_ndc_gt) ** 2
+        return lm_2d_ndc - lm_2d_ndc_gt
 
 
 class Landmark3DLoss(BaseLoss):
+    name: str = "landmark3d"
+
     def forward(self, out: dict[str, torch.Tensor]):
         lm_3d_camera = out["lm_3d_camera"]
         lm_3d_camera_gt = out["lm_3d_camera_gt"]
-        return (lm_3d_camera - lm_3d_camera_gt) ** 2
+        return lm_3d_camera - lm_3d_camera_gt

lib/optimizer/newton.py
@@ -66,33 +66,34 @@ class LevenbergMarquardt(BaseOptimizer):
     def get_state(self):
         return {"damping_factor": self.damping_factor}
 
-    def solve_delta(self, JTJ: torch.tensor, JTF: torch.Tensor, damping_factor: float):
+    def solve_delta(self, H: torch.tensor, grad_f: torch.Tensor, damping_factor: float):
         """Apply the hessian approximation and solve for the delta"""
-        D = torch.diag(torch.ones_like(torch.diag(JTJ)))
+        D = torch.diag(torch.ones_like(torch.diag(H)))
         if not self.levenberg:
-            D = torch.diag(torch.diag(JTJ))
+            D = torch.diag(torch.diag(H))
 
         # build the matrix and solve for the delta
-        A = 2 * JTJ + damping_factor * D
-        direction = self.lin_solver(A=A, b=JTF)
+        A = H + damping_factor * D
+        delta = self.lin_solver(A=A, b=grad_f)
+        direction = -delta  # we need to go the negative direction
 
-        self.save_system(A=A, x=direction, b=JTF)
+        self.save_system(A=A, x=delta, b=grad_f)
         return direction
 
     def dynamic_solve(
         self,
-        JTJ: torch.Tensor,
-        JTF: torch.Tensor,
+        H: torch.Tensor,
+        grad_f: torch.Tensor,
         loss_closure: Callable[[], torch.Tensor],
     ):
         # prepare the init delta vectors
         x_init = self._clone_param()
         loss = loss_closure()
 
-        dx = self.solve_delta(JTJ, JTF, damping_factor=self.damping_factor)
+        dx = self.solve_delta(H, grad_f, damping_factor=self.damping_factor)
         loss_df = self._evaluate(loss_closure, x_init, self.step_size, dx)
         df_factor = self.damping_factor / self.factor
-        dx_factor = self.solve_delta(JTJ, JTF, damping_factor=df_factor)
+        dx_factor = self.solve_delta(H, grad_f, damping_factor=df_factor)
         loss_df_factor = self._evaluate(loss_closure, x_init, self.step_size, dx_factor)
 
         # both are worse -> increase damping factor until improvement
@@ -100,7 +101,7 @@ class LevenbergMarquardt(BaseOptimizer):
             improvement = False
             for k in range(1, self.max_damping_steps + 1):
                 df_k = self.damping_factor * (self.factor**k)
-                dx_k = self.solve_delta(JTJ, JTF, damping_factor=df_k)
+                dx_k = self.solve_delta(H, grad_f, damping_factor=df_k)
                 loss_dx_k = self._evaluate(loss_closure, x_init, self.step_size, dx_k)
                 if loss_dx_k <= loss:  # improvement or same (converged)
                     improvement = True
@@ -122,23 +123,23 @@ class LevenbergMarquardt(BaseOptimizer):
 
     def static_solve(
         self,
-        JTJ: torch.Tensor,
-        JTF: torch.Tensor,
+        H: torch.Tensor,
+        grad_f: torch.Tensor,
         loss_closure: Callable[[], torch.Tensor],
     ):
         loss = loss_closure()
-        dx = self.solve_delta(JTJ, JTF, damping_factor=self.damping_factor)
+        dx = self.solve_delta(H, grad_f, damping_factor=self.damping_factor)
         # loss_df = self._evaluate(loss_closure, x_init, self.step_size, dx)
         # if loss_df > loss:
         #     return torch.zeros_like(dx), loss  # ensure that the we converge
-        return dx, loss
+        return -dx, loss
 
     def apply_jacobian(self, jacobian_closure: Callable[[], torch.Tensor]):
         J, F = jacobian_closure()  # (M, N)
         assert J.shape[1] == self._numel
-        JTJ = J.T @ J  # (N, N)
-        JTF = J.T @ F
-        return JTJ, -JTF
+        H = 2 * J.T @ J  # (N, N)
+        grad_f = 2 * J.T @ F
+        return H, grad_f
 
     @torch.no_grad()
     def step(
@@ -147,24 +148,24 @@ class LevenbergMarquardt(BaseOptimizer):
         jacobian_closure: Callable[[], torch.Tensor],
     ) -> float:
         # prepare the init delta vectors
-        JTJ, JTF = self.apply_jacobian(jacobian_closure)
+        H, grad_f = self.apply_jacobian(jacobian_closure)
 
         # difference with JTF
         with torch.enable_grad():
             self._zero_grad()
             loss = loss_closure()
             loss.backward()
-            grad = self._gather_flat_grad().neg()
+            grad = self._gather_flat_grad()
             if self.use_grad:
-                JTF = grad  # dont use the pytorch gradients
+                grad_f = grad  # dont use the pytorch gradients
 
         x_init = self._clone_param()
 
         # solve for the delta
         if self.mode == "static":
-            direction, loss = self.static_solve(JTJ, JTF, loss_closure)
+            direction, loss = self.static_solve(H, grad_f, loss_closure)
         elif self.mode == "dynamic":
-            direction, loss = self.dynamic_solve(JTJ, JTF, loss_closure)
+            direction, loss = self.dynamic_solve(H, grad_f, loss_closure)
         else:
             ValueError(f"The mode={self.mode} is not possible.")
 